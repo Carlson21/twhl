340d5be6639ed71027978e4954676945b79c2f83
Carlson21 <45288359+Carlson21@users.noreply.github.com>
2020-10-31 12:31:11 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,21 @@
+import telebot
+import time
+import os
+
+#url = "https://api.telegram.org/bot1250883576:AAFe-Wp9Uk4lVRX3syccH6ALbs91bsBSkXA/"
+TOKEN = "1250883576:AAFe-Wp9Uk4lVRX3syccH6ALbs91bsBSkXA"
+bot = telebot.TeleBot(TOKEN)
+
+def send_clip(clip_path, nick):
+    clip = open(clip_path, 'rb')
+    bot.send_message(285999952, 'aaaaaaa')
+    bot.send_video(285999952, clip, caption=nick, timeout=100)
+    clip.close()
+    os.remove(clip_path)
+    print("\nSENT AND REMOVED {}".format(clip_path))
+    time.sleep(1800)
+
+
+
+# channel -1001454098114
+# me 285999952
