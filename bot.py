d9f9021d8a17a1ff92389a401f358712c138b73c
Carlson21 <45288359+Carlson21@users.noreply.github.com>
2020-10-31 13:32:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,19 +2,17 @@ import telebot
 import time
 import os
 
-TOKEN = "1250883576:AAFe-Wp9Uk4lVRX3syccH6ALbs91bsBSkXA"
+TOKEN = os.getenv("TOKEN")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+
 bot = telebot.TeleBot(TOKEN)
 
 def send_clip(clip_path, nick):
     clip = open(clip_path, 'rb')
-    bot.send_message(285999952, 'aaaaaaa')
-    bot.send_video(285999952, clip, caption=nick, timeout=100)
+    bot.send_video(CHANNEL_ID, clip, caption=nick, timeout=100)
     clip.close()
     os.remove(clip_path)
     print("\nSENT AND REMOVED {}".format(clip_path))
     time.sleep(1800)
 
 
-
-# channel -1001454098114
-# me 285999952
